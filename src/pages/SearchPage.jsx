4556ef7d4e4887917b7202b9a41dc2d98037a4ef
uncleoptimus <uncle.optimus@gmail.com>
2017-08-06 19:26:38 -0700

    Add basic component blocks to Search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,17 +1,77 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { withStyles, createStyleSheet } from 'material-ui/styles';
-// import Grid from 'material-ui/Grid';
-// import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
 
-const SearchPage = () =>
-  <div>
-    <Typography type="display4" color="inherit">
-      Sarch.
-    </Typography>
-  </div>;
+import Shelf from '../modules/Shelf';
 
-export default SearchPage;
+const styleSheet = createStyleSheet(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+    // width: 200,
+  }
+}));
+
+class SearchPage extends Component {
+  state = {
+    books: []
+  };
+
+  assignToShelf = (targetBook: any, shelfType: string) => {
+    console.log('assign this book to a shelf!');
+    // BooksAPI.update(targetBook, shelfType).then(response => {
+    //   const books = this.state.books.slice();
+    //   const targetBookIndex = books.findIndex(
+    //     book => book.id === targetBook.id
+    //   );
+    //   books[targetBookIndex] = Object.assign({}, targetBook, {
+    //     shelf: shelfType
+    //   });
+    //   this.setState({ books });
+    // });
+  };
+
+  render() {
+    const { books } = this.state;
+
+    const { classes } = this.props;
+
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography type="display4" color="inherit">
+            Search!
+          </Typography>
+          <TextField
+            id="margin-normal"
+            className={classes.textField}
+            placeholder="?"
+            margin="normal"
+            fullWidth
+          />
+          {books.length
+            ? <Shelf
+                items={books}
+                label="results:"
+                onChange={this.assignToShelf}
+              />
+            : <Typography type="display1" color="inherit">
+                nothing yet. type something?
+              </Typography>}
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+// export default SearchPage;
+export default withStyles(styleSheet)(SearchPage);
